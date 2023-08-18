56d371103c52c2c8034d7064be5afd82cbd533e5
Hanui Jang <hany0147@naver.com>
2023-08-18 15:58:45 +0900

    [230818]

---
+++ Node_Js/chapter9/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config(); // process.env
+const pageRouter = require('./routes/page');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure(templatePath: 'views', opts: {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan(format: 'dev'));
+app.use
