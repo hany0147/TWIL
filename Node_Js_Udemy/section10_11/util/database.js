e59abd817f1e9201645570efd3054ec07351e6ec
Hanui Jang <hany0147@naver.com>
2023-12-22 19:55:22 +0900

    [231222]

---
+++ Node_Js_Udemy/section10_11/util/database.js
@@ -0,0 +1,18 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'node-complete',
+//   password: '패스워드'
+// });
+
+// module.exports = pool.promise();
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "패스워드", {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports = sequelize;
